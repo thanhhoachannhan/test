95d5bcbd1c533ae46b881937d86b7b5c47539ec2
thanhhoachannhan <155319976+thanhhoachannhan@users.noreply.github.com>
2024-09-09 17:46:11 +0700

    up

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ TEMPLATES = [{
     'APP_DIRS': True,
     'OPTIONS': {'context_processors': [f'django.contrib.{x}.context_processors.{x}' for x in ['auth','messages']]}
 }]
-INSTALLED_APPS += ['rest_framework', 'rest_framework_simplejwt.token_blacklist']
 SILENCED_SYSTEM_CHECKS = ['admin.W411']
 signal.signal(signal.SIGINT, lambda sig, frame: (shutil.rmtree('__pycache__', ignore_errors=True), sys.exit(0)))
+INSTALLED_APPS += ['rest_framework', 'rest_framework_simplejwt.token_blacklist']
+REST_FRAMEWORK = {'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_simplejwt.authentication.JWTAuthentication']}
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
