be6e2a6bd9a03e4d61ec59a0a7257e15fbdff0b7
thanhhoachannhan <155319976+thanhhoachannhan@users.noreply.github.com>
2024-09-10 17:17:21 +0700

    up

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE','app')
 if __name__ == '__main__': exec.execute_from_command_line(sys.argv)
 BASE_DIR=pathlib.Path(__file__).resolve().parent;SECRET_KEY='X';DEBUG=True;ROOT_URLCONF='urls';STATIC_URL='static/'
 DATABASES={'default':{'ENGINE':'django.db.backends.sqlite3','NAME':'db.sqlite3'}}
-INSTALLED_APPS=[f'django.contrib.{x}' for x in 'admin,auth,sessions,contenttypes,messages,staticfiles'.split(',')] + ['rest_framework', 'rest_framework_simplejwt.token_blacklist']
+INSTALLED_APPS=[f'django.contrib.{x}' for x in 'admin,auth,sessions,contenttypes,messages,staticfiles'.split(',')]+['rest_framework', 'rest_framework_simplejwt.token_blacklist']
 MIDDLEWARE=[f'django.contrib.{a}.middleware.{b}Middleware' for a,b in [('sessions','Session'),('auth','Authentication'),('messages','Message')]]
 TEMPLATES=[{
     'BACKEND':'django.template.backends.django.DjangoTemplates','DIRS':[BASE_DIR],'APP_DIRS':True,
