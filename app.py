e7d5d6208d96256d71e281add30bd3e9e6ebc27a
thanhhoachannhan <155319976+thanhhoachannhan@users.noreply.github.com>
2024-09-12 18:00:42 +0700

    up

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ DATABASES={'default':{'ENGINE':'django.db.backends.sqlite3','NAME':'db.sqlite3'}
 INSTALLED_APPS=[f'django.contrib.{x}' for x in 'admin,auth,sessions,contenttypes,messages,staticfiles'.split(',')]+['rest_framework','rest_framework_simplejwt.token_blacklist']
 MIDDLEWARE=[f'django.contrib.{a}.middleware.{b}Middleware' for a,b in [('sessions','Session'),('auth','Authentication'),('messages','Message')]]
 TEMPLATES=[{'BACKEND':'django.template.backends.django.DjangoTemplates','DIRS':[BASE],'APP_DIRS':True,'OPTIONS':{'context_processors':[f'django.contrib.{x}.context_processors.{x}' for x in ['auth','messages']]}}]
+REST_FRAMEWORK = {'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_simplejwt.authentication.JWTAuthentication']}
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SILENCED_SYSTEM_CHECKS=['admin.W411']
+import sys,signal,shutil; signal.signal(signal.SIGINT, lambda sig, frame: (shutil.rmtree('__pycache__', ignore_errors=True), sys.exit(0)))
